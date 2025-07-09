fe60bc6789716ab23b4785b5936615e4520c8c45
Kossiba <150734694+Kossiba@users.noreply.github.com>
2025-07-09 17:34:20 -0300

    Update user.js

--- mvc-app/src/models/user.js
+++ mvc-app/src/models/user.js
@@ -1,14 +1,14 @@
-
 // src/models/user.js
 
 /**
- * Cria um objeto “user” com os campos login e senha (strings).
- * @param {{ login?: any, senha?: any }} data
- * @returns {{ login: string, senha: string }}
+ * Cria um objeto “user” com os campos login, senha e email (strings).
+ * @param {{ login?: any, senha?: any, email?: any }} data
+ * @returns {{ login: string, senha: string, email: string }}
  */
-export function createUser({ login = '', senha = '' } = {}) {
+export function createUser({ login = '', senha = '', email = '' } = {}) {
   return {
     login: String(login),
-    senha:  String(senha)
+    senha:  String(senha),
+    email:  String(email)
   };
 }
